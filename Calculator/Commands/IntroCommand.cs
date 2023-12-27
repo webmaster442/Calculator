88e673583ae5909c643001d8406cab225a702f74
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2023-12-27 15:22:08 +0100

    Versioning

--- Calculator/Commands/IntroCommand.cs
+++ Calculator/Commands/IntroCommand.cs
@@ -1,12 +1,20 @@
-﻿using CalculatorShell.Core;
+﻿using System.Net;
+
+using CalculatorShell.Core;
 
 namespace Calculator.Commands;
 
 internal class IntroCommand : IAutoExecShellCommand
 {
+    private static Version GetVersion()
+    {
+        return typeof(IntroCommand).Assembly.GetName().Version 
+            ?? new Version(9999, 9999, 9999, 9999);
+    }
+
     public void Execute(IHost host)
     {
-        host.Output.MarkupLine("Welcome to [blue]Calculator[/]");
+        host.Output.MarkupLine($"[blue]Calculator {GetVersion()}[/]");
         host.Output.MarkupLine("To list commands type [lime]commands[/], to list supported functions type: [green]functions[/]");
         host.Output.MarkupLine("To exit type [yellow]exit[/] or [yellow]quit[/]");
         host.Output.BlankLine();
