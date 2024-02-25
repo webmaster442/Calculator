3dd288cc1e9bd53009057a7b8a4cf98cc9ea16f8
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-25 16:39:17 +0100

    Integrates UI kit

---
+++ Calculator/RequestHandlers/BuiltInFilesRequestHandler.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Net;
+using System.Net.Mime;
+
+using Calculator.Internal;
+using Calculator.Resources;
+using Calculator.Web.Server;
+
+namespace Calculator.RequestHandlers;
+
+internal sealed class BuiltInFilesRequestHandler : IRequestHandler
+{
+    public bool HandleRequest(HttpListenerContext context)
+    {
+        if (context.IsMatch("GET", "/favicon.ico"))
+            return Serve(context, ResourceNames.Icon, MediaTypeNames.Image.Icon);
+
+        else if (context.IsMatch("GET", "/uikit.min.css"))
+            return Serve(context, ResourceNames.UiKitCss, MediaTypeNames.Text.Css);
+
+        else if (context.IsMatch("GET", "/uikit.min.js"))
+            return Serve(context, ResourceNames.UiKitJs, MediaTypeNames.Text.JavaScript);
+
+        else if (context.IsMatch("GET", "/uikit-icons.min.js"))
+            return Serve(context, ResourceNames.UiKitIconJs, MediaTypeNames.Text.JavaScript);
+
+        return false;
+    }
+
+    private static bool Serve(HttpListenerContext context, string resourceName, string mime)
+    {
+        using var iconStream = Helpers.GetResourceStream(resourceName);
+        context.Tranfer(iconStream, mime, HttpStatusCode.OK);
+        return true;
+    }
+}
--- Calculator/Resources/ResourceNames.cs
+++ Calculator/Resources/ResourceNames.cs
@@ -10,5 +10,9 @@ internal static class ResourceNames
     public const string TemplateHtml = "Calculator.Resources.Template.html";
     public const string ManualMd = "Calculator.manual.md";
     public const string TerminalFragmentJson = "Calculator.Resources.TerminalFragment.json";
+    
     public const string Icon = "Calculator.Calculator.ico";
+    public const string UiKitCss = "Calculator.Resources.uikit.min.css";
+    public const string UiKitJs = "Calculator.Resources.uikit.min.js";
+    public const string UiKitIconJs = "Calculator.Resources.uikit-icons.min.js";
 }
