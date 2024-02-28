e47b9112187d37cc607d631811888c3279d7cf22
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-28 19:25:39 +0100

    Preliminary time command implementations

---
+++ Calculator/Commands/TimeAddCommand.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Diagnostics;
+
+using CalculatorShell.Core;
+
+namespace Calculator.Commands;
+
+internal sealed class TimeAddCommand : TimeCommand
+{
+    internal enum AddSubtractUnit
+    {
+        Hour,
+        Minute,
+        Second,
+        Day,
+        Month,
+        Year,
+    }
+
+    private DateTime AddSubtractDates(DateTime start, string value, AddSubtractUnit unit)
+    {
+        return unit switch
+        {
+            AddSubtractUnit.Second => start.AddSeconds(double.Parse(value, Host.CultureInfo)),
+            AddSubtractUnit.Minute => start.AddMinutes(double.Parse(value, Host.CultureInfo)),
+            AddSubtractUnit.Hour => start.AddHours(double.Parse(value, Host.CultureInfo)),
+            AddSubtractUnit.Day => start.AddDays(double.Parse(value, Host.CultureInfo)),
+            AddSubtractUnit.Month => start.AddMonths(int.Parse(value, Host.CultureInfo)),
+            AddSubtractUnit.Year => start.AddYears(int.Parse(value, Host.CultureInfo)),
+            _ => throw new UnreachableException(),
+        };
+    }
+
+    public TimeAddCommand(IHost host) : base(host)
+    {
+    }
+
+    public override string[] Names => ["time-add"];
+
+    public override string Synopsys
+        => "Add or subtract from a given date/time value";
+
+    public override void ExecuteInternal(Arguments args)
+    {
+        args.ThrowIfNotSpecifiedAtLeast(3);
+        DateTime start = Parse(args[0]);
+        AddSubtractUnit unit = args.Parse<AddSubtractUnit>(2);
+        DateTime result = AddSubtractDates(start, args[1], unit);
+        Host.Output.Result(FormatTime(result));
+    }
+}
---
+++ Calculator/Commands/TimeDiffCommand.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using CalculatorShell.Core;
+
+namespace Calculator.Commands;
+
+internal sealed class TimeDiffCommand : TimeCommand
+{
+    public TimeDiffCommand(IHost host) : base(host)
+    {
+    }
+
+    public override string[] Names => ["time-diff"];
+
+    public override string Synopsys
+        => "Calculates the difference between two Date/time values";
+
+    public override void ExecuteInternal(Arguments args)
+    {
+        args.ThrowIfNotSpecifiedAtLeast(2);
+
+        DateTime timeA = Parse(args[0]);
+        DateTime timeB = Parse(args[1]);
+
+        TimeSpan result = timeA - timeB;
+
+        Host.Output.Result(result.ToString());
+    }
+}
