19598f30203629e7daefde1a33a4e5b27a2ad460
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-03-05 20:39:39 +0100

    License header update

--- Calculator/OptionsBase.cs
+++ Calculator/OptionsBase.cs
@@ -1,4 +1,9 @@
-﻿namespace Calculator;
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+namespace Calculator;
 
 internal class OptionsBase
 {
--- CalculatorShell.Core/ShellCommand.cs
+++ CalculatorShell.Core/ShellCommand.cs
@@ -3,7 +3,6 @@
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
-
 namespace CalculatorShell.Core;
 
 public abstract class ShellCommand : ISyncShellCommand
