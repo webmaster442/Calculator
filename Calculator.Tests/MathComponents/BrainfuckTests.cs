17bdfcbaad55a16638062c8a9cef58fb79e0b08c
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-04-16 13:16:22 +0200

    Brainfuck encoder

--- Calculator.Tests/MathComponents/BrainfuckTests.cs
+++ Calculator.Tests/MathComponents/BrainfuckTests.cs
@@ -15,4 +15,25 @@ internal class BrainfuckTests
         Brainfuck.Interpret(io, code);
         Assert.That(io.ToString(), Is.EqualTo(expected));
     }
+
+    [TestCase("ABC", "++++++++[>+>++>+++>++++>+++++>++++++>+++++++>++++++++>+++++++++>++++++++++>+++++++++++>++++++++++++>+++++++++++++>++++++++++++++>+++++++++++++++>++++++++++++++++<<<<<<<<<<<<<<<<-]>>>>>>>>+.-<<<<<<<<>>>>>>>>++.--<<<<<<<<>>>>>>>>+++.---<<<<<<<<.")]
+    [TestCase("99", "++++++++[>+>++>+++>++++>+++++>++++++>+++++++>++++++++>+++++++++>++++++++++>+++++++++++>++++++++++++>+++++++++++++>++++++++++++++>+++++++++++++++>++++++++++++++++<<<<<<<<<<<<<<<<-]>>>>>>>+.-<<<<<<<>>>>>>>+.-<<<<<<<.")]
+    [TestCase("0", "++++++++[>+>++>+++>++++>+++++>++++++>+++++++>++++++++>+++++++++>++++++++++>+++++++++++>++++++++++++>+++++++++++++>++++++++++++++>+++++++++++++++>++++++++++++++++<<<<<<<<<<<<<<<<-]>>>>>>.<<<<<<.")]
+    [TestCase("", "")]
+    public void EncodeTest(string input, string expected)
+    {
+        var result = Brainfuck.Encode(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase("Hello")]
+    [TestCase("")]
+    [TestCase("123456789")]
+    public void EncodeInterpretRoundTripTest(string input)
+    {
+        var code = Brainfuck.Encode(input);
+        var io = new StringBuilderBrainfuckIo();
+        Brainfuck.Interpret(io, code);
+        Assert.That(io.ToString().TrimEnd('\0'), Is.EqualTo(input));
+    }
 }
--- CalculatorShell.Engine/MathComponents/Brainfuck.cs
+++ CalculatorShell.Engine/MathComponents/Brainfuck.cs
@@ -1,12 +1,18 @@
-﻿using System.Data;
-using System.Text;
-
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Text;
 
 namespace CalculatorShell.Engine.MathComponents;
 
 public static class Brainfuck
 {
+    private const char MemorySlotIncement = '>';
+    private const char MemorySlotDecrement = '<';
+    private const char ValueIncrement = '+';
+    private const char ValueDecrement = '-';
+    private const char Output = '.';
+    private const char Input = ',';
+    private const char LoopStart = '[';
+    private const char LoopEnd = ']';
+
     public static void Interpret(IBrainFuckIO iO, string code)
     {
         int codePointer = 0;
@@ -17,29 +23,29 @@ public static class Brainfuck
         {
             switch (code[codePointer])
             {
-                case '>':
+                case MemorySlotIncement:
                     memoryPointer++;
                     if (memoryPointer > memory.Length)
                         throw new EngineException("Brainfuck memory overflow");
                     break;
-                case '<':
+                case MemorySlotDecrement:
                     memoryPointer--;
                     if (memoryPointer < 0)
                         throw new EngineException("Brainfuck memory underflow");
                     break;
-                case '+':
+                case ValueIncrement:
                     memory[memoryPointer]++;
                     break;
-                case '-':
+                case ValueDecrement:
                     memory[memoryPointer]--;
                     break;
-                case '.':
+                case Output:
                     iO.Print(memory[memoryPointer]);
                     break;
-                case ',':
+                case Input:
                     memory[memoryPointer] = iO.Read();
                     break;
-                case '[':
+                case LoopStart:
                     if (memory[memoryPointer] == 0)
                     {
                         int loopCount = 1;
@@ -57,7 +63,7 @@ public static class Brainfuck
                         }
                     }
                     break;
-                case ']':
+                case LoopEnd:
                     if (memory[memoryPointer] != 0)
                     {
                         int loopCount = 1;
@@ -78,5 +84,73 @@ public static class Brainfuck
             }
             codePointer++;
         }
+    }
+
+    private static string Encode(int c)
+    {
+        var char_code = new StringBuilder();
+        var slot = c / 8;
+        for (var i = 0; i < slot; i++)
+        {
+            char_code.Append(MemorySlotIncement);
+        }
+
+        int pointer_diff;
+        char pointer = ValueIncrement;
+        char pointer_reversed = ValueDecrement;
+
+        // set the pointer
+        if (c > (slot * 8))
+        {
+            pointer_diff = c - (slot * 8);
+            pointer = ValueIncrement;
+            pointer_reversed = ValueDecrement;
+        }
+        else if (c < (slot * 8))
+        {
+            pointer_diff = (slot * 8) - c;
+            pointer = ValueDecrement;
+            pointer_reversed = ValueIncrement;
+        }
+        else
+        {
+            pointer_diff = 0;
+        }
+        for (var i = 0; i < pointer_diff; i++)
+        {
+            char_code.Append(pointer);
+        }
+        // output the character
+        char_code.Append(Output);
+
+        // reset the pointer
+        for (var i = 0; i < pointer_diff; i++)
+        {
+            char_code.Append(pointer_reversed);
+        }
+        // reset the slot
+        for (var i = 0; i < slot; i++)
+        {
+            char_code.Append(MemorySlotDecrement);
+        }
+        // return our bf code
+        return char_code.ToString();
+    }
+
+    public static string Encode(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return string.Empty;
+
+        if (s.Any(x => x > 128))
+            throw new EngineException("Only ASCII input is supported");
+
+        StringBuilder final = new("++++++++[>+>++>+++>++++>+++++>++++++>+++++++>++++++++>+++++++++>++++++++++>+++++++++++>++++++++++++>+++++++++++++>++++++++++++++>+++++++++++++++>++++++++++++++++<<<<<<<<<<<<<<<<-]");
+        for (var i = 0; i < s.Length; i++)
+        {
+            final.Append(Encode(s[i]));
+        }
+        final.Append(Output);
+        return final.ToString();
     }
 }
